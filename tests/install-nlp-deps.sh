0ebe884ce48a91f07845c45b90295d545048da5c
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-12-14 21:03:50 -0800

    Switch to next branch of genie-toolkit, and update thingtalk to 2.0.0

--- tests/install-nlp-deps.sh
+++ tests/install-nlp-deps.sh
@@ -7,7 +7,7 @@ set -o pipefail
 srcdir=`dirname $0`/..
 srcdir=`realpath $srcdir`
 
-which genienlp >/dev/null 2>&1 || pip3 install --user genienlp==0.4.0b1
+which genienlp >/dev/null 2>&1 || pip3 install --user 'git+https://github.com/stanford-oval/genienlp@10b51e4623a4e8cc0ec893a37507f26393244c31#egg=genienlp'
 which genienlp
 
 mkdir -p $srcdir/tests/embeddings
